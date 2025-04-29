231ccff33cbf52290ac82b097fdc43434e6188f2
Alish74 <serikalisher31@gmail.com>
2025-04-30 00:26:15 +0500

    Добавлена модель Task и шаблон home

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
-# Register your models here.
+from .models import Task
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+ list_display = ('title', 'created_at') # Поля, которые будут отображаться в списке
+ search_fields = ('title',) # Поиск по названию задачи
+ list_filter = ('created_at',) # Фильтр по дате создания
